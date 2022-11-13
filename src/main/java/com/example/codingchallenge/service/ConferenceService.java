49007d4e35a2f2bfb00800a46a0bd02c5214cb18
greszter <esztergreskovics@gmail.com>
2022-11-13 10:05:48 +0100

    Add getSchedule method to create final schedule and return LinkedHashMap of talks instead of ArrayList

--- src/main/java/com/example/codingchallenge/service/ConferenceService.java
+++ src/main/java/com/example/codingchallenge/service/ConferenceService.java
@@ -3,8 +3,11 @@ package com.example.codingchallenge.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @Service
@@ -16,9 +19,16 @@ public class ConferenceService {
     @Autowired
     private SectionCreationHelper helper;
 
-    public List<String> createSchedule(List<String> input) {
+
+    private final int MORNING_SECTION_LENGTH = 180;
+    private final int AFTERNOON_SECTION_LENGTH = 240;
+    private int trackCount = 1;
+
+    public LinkedHashMap<String, ArrayList<String>> createSchedule(List<String> input) {
+        var schedule = new LinkedHashMap<String, ArrayList<String>>();
+
         if (input.isEmpty()) {
-            return input;
+            return schedule;
         }
 
         var talksWithLength = new HashMap<String, Integer>();
@@ -28,8 +38,42 @@ public class ConferenceService {
             talksWithLength.put(talk, length);
         });
 
-        var schedule = new ArrayList<String>();
+        while (!talksWithLength.isEmpty()) {
+            getSchedule(talksWithLength, schedule);
+        }
 
         return schedule;
+    }
+
+    private void getSchedule(HashMap<String, Integer> talksWithLength, LinkedHashMap<String, ArrayList<String>> schedule) {
+        var morningSection = helper.createSection(talksWithLength, MORNING_SECTION_LENGTH);
+        var afternoonSection = helper.createSection(talksWithLength, AFTERNOON_SECTION_LENGTH);
+        var morningSectionTitles = morningSection.keySet().toArray(new String[morningSection.size()]);
+        var afternoonSectionTitles = afternoonSection.keySet().toArray(new String[afternoonSection.size()]);
+        var time = LocalTime.parse("09:00", DateTimeFormatter.ofPattern("HH:mm"));
+        var talkList = new ArrayList<String>();
+
+        for (String title : morningSectionTitles) {
+            var length = morningSection.get(title);
+            String titleWithTime = time + "AM " + title;
+
+            talkList.add(titleWithTime);
+            time = time.plusMinutes(length);
+        }
+
+        talkList.add("12:00PM Lunch");
+        time = LocalTime.parse("13:00", DateTimeFormatter.ofPattern("HH:mm"));
+
+        for (String title : afternoonSectionTitles) {
+            var length = afternoonSection.get(title);
+            String titleWithTime = time + "PM " + title;
+
+            talkList.add(titleWithTime);
+            time = time.plusMinutes(length);
+        }
+
+        schedule.put("Track " + trackCount, talkList);
+
+        trackCount++;
     }
 }
--- src/test/java/com/example/codingchallenge/ConferenceServiceTest.java
+++ src/test/java/com/example/codingchallenge/ConferenceServiceTest.java
@@ -1,19 +1,15 @@
 package com.example.codingchallenge;
 
 import com.example.codingchallenge.service.ConferenceService;
-import com.example.codingchallenge.service.TalkLengthAnalyzer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ConferenceServiceTest {
@@ -21,18 +17,14 @@ class ConferenceServiceTest {
 	@Autowired
 	private ConferenceService service;
 
-	@MockBean
-	private TalkLengthAnalyzer analyzer;
-
 	@Test
 	@DisplayName("Conference service return list of strings on proper input")
 	public void testCreateListReturnsList() throws Exception {
 		var input = getInput();
-		when(analyzer.getTalkLength(anyString())).thenReturn(anyInt());
 
 		var schedule = service.createSchedule(input);
 
-		assertThat(schedule.size()).isEqualTo(0);
+		assertThat(schedule.size()).isEqualTo(2);
 	}
 
 	public List<String> getInput() {
@@ -40,6 +32,23 @@ class ConferenceServiceTest {
 
 		input.add("Writing Fast Tests Against Enterprise Rails 60min");
 		input.add("Overdoing it in Python 45min");
+		input.add("Lua for the Masses 30min");
+		input.add("Ruby Errors from Mismatched Gem Versions 45min");
+		input.add("Common Ruby Errors 45min");
+		input.add("Rails for Python Developers lightning");
+		input.add("Communicating Over Distance 60min");
+		input.add("Accounting-Driven Development 45min");
+		input.add("Woah 30min");
+		input.add("Sit Down and Write 30min");
+		input.add("Pair Programming vs Noise 45min");
+		input.add("Rails Magic 60min");
+		input.add("Ruby on Rails: Why We Should Move On 60min");
+		input.add("Clojure Ate Scala (on my project) 45min");
+		input.add("Programming in the Boondocks of Seattle 30min");
+		input.add("Ruby vs. Clojure for Back-End Development 30min");
+		input.add("Ruby on Rails Legacy App Maintenance 60min");
+		input.add("A World Without HackerNews 30min");
+		input.add("User Interface CSS in Rails Apps 30min");
 
 		return input;
 	}
